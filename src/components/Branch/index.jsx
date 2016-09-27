17191ff4feb6e67d1a838b1a42092a02756c047d
Chris Trevino <darthtrevino@gmail.com>
2016-09-27 15:15:58 -0700

    Remove unused EditBranch component

--- src/components/Branch/index.jsx
+++ src/components/Branch/index.jsx
@@ -1,78 +1,45 @@
 import React, { PropTypes } from 'react';
 import classnames from 'classnames';
 import BranchProfile from '../BranchProfile';
-import EditBranch from './EditBranch';
 import './Branch.scss';
 
 const DO_NOTHING = () => ({});
 
-class Branch extends React.Component {
-  constructor() {
-    super();
-    this.state = { editMode: false };
-  }
-
-  onClickEdit() {
-    this.setState({ editMode: true });
-  }
-
-  onDoneEditing() {
-    this.setState({ editMode: false });
-  }
-
-  onChanged(payload) {
-    this.props.onRename(payload);
-  }
-
-  render() {
-    const {
-      label,
-      branchType,
-      startsAt,
-      endsAt,
-      currentBranchStart,
-      currentBranchEnd,
-      maxDepth,
-      activeStateIndex,
-      onClick,
-      active,
-      successorDepth,
-      pinnedStateIndex,
-    } = this.props;
-    const {
-      editMode,
-    } = this.state;
-
-    return editMode ? (
-      <EditBranch
-        {...this.props}
-        onDoneEditing={() => this.onDoneEditing()}
-        onRename={p => this.onChanged(p)}
+const Branch = ({
+  label,
+  branchType,
+  startsAt,
+  endsAt,
+  currentBranchStart,
+  currentBranchEnd,
+  maxDepth,
+  activeStateIndex,
+  onClick,
+  active,
+  successorDepth,
+  pinnedStateIndex,
+}) => (
+  <div className="history-branch" onClick={onClick || DO_NOTHING}>
+    <div className="history-branch-profile-container">
+      <BranchProfile
+        start={startsAt}
+        end={endsAt}
+        max={maxDepth}
+        pinnedStateIndex={pinnedStateIndex}
+        branchStart={currentBranchStart}
+        branchEnd={currentBranchEnd}
+        type={branchType}
+        successorStateIndex={successorDepth}
+        activeStateIndex={activeStateIndex}
       />
-    ) : (
-      <div className="history-branch" onClick={onClick || DO_NOTHING}>
-        <div className="history-branch-profile-container">
-          <BranchProfile
-            start={startsAt}
-            end={endsAt}
-            max={maxDepth}
-            pinnedStateIndex={pinnedStateIndex}
-            branchStart={currentBranchStart}
-            branchEnd={currentBranchEnd}
-            type={branchType}
-            successorStateIndex={successorDepth}
-            activeStateIndex={activeStateIndex}
-          />
-        </div>
-        <div className="branch-details">
-          <div className={classnames('branch-name', { active })}>
-            {label}
-          </div>
-        </div>
+    </div>
+    <div className="branch-details">
+      <div className={classnames('branch-name', { active })}>
+        {label}
       </div>
-    );
-  }
-}
+    </div>
+  </div>
+);
 
 Branch.propTypes = {
   label: PropTypes.string.isRequired,
