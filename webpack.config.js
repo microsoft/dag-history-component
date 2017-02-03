dd9d890447c1e44a5505313c1a5c10271efc46b4
Chris Trevino <darthtrevino@gmail.com>
2017-02-03 14:12:00 -0800

    Add skeleton-css dep; remove index.html, use webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,34 @@
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
+const CSS_LOADERS = ['style-loader', 'css-loader', 'postcss-loader'];
+const NODE_MODULES = path.join(__dirname, 'node_modules');
+
 module.exports = {
+  devtool: 'source-map',
   context: path.join(__dirname),
-  devtool: 'inline-source-map',
   entry: {
     javascript: './example/app.tsx',
-    html: './example/index.html',
   },
-  output: {
-    filename: './appbundle.js',
-  },
+  output: './dist/appbundle.js',
   resolve: {
     extensions: ['', '.js', '.jsx', '.ts', '.tsx'],
-    modulesDirectories: [path.join(__dirname, 'node_modules')],
-    fallback: path.join(__dirname, 'node_modules'),
+    modulesDirectories: [NODE_MODULES],
+    fallback: NODE_MODULES,
     alias: {
       'react/lib/ReactMount': 'react-dom/lib/ReactMount',
       sinon: 'sinon/pkg/sinon',
     },
   },
   resolveLoader: {
-    modulesDirectories: [path.join(__dirname, 'node_modules')],
-    fallback: path.join(__dirname, 'node_modules'),
+    modulesDirectories: [NODE_MODULES],
+    fallback: NODE_MODULES,
   },
   plugins: [
+    new HtmlWebpackPlugin({
+      title: 'DAG History Component Example',
+    }),
     new webpack.NoErrorsPlugin(),
     new webpack.ProvidePlugin({
       saveAs: 'imports?this=>global!exports?global.saveAs!filesaver.js',
@@ -32,9 +36,8 @@ module.exports = {
   ],
   module: {
     loaders: [
-        { test: /\.html$/, loader: 'file-loader?name=[name].[ext]' },
-        { test: /\.css$/, loader: 'style-loader!css-loader!postcss-loader' },
-        { test: /\.scss$/, loader: 'style-loader!css-loader!postcss-loader!sass-loader' },
+        { test: /\.css$/, loaders: CSS_LOADERS },
+        { test: /\.scss$/, loaders: [...CSS_LOADERS, 'sass-loader'] },
         { test: /\.ts(x|)/, loaders: ['ts-loader'], exclude: /node_modules/ },
     ],
   },
